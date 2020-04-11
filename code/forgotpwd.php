bd97bc07d296df871c0a178589d54c1610781906
SurrenderAt20 <lassevfuglsbjerg@gmail.com>
2020-04-11 16:10:28 +0200

    PHPMailer?
    PHPMailer skal installeres og bruges som et redskab for at kunne sende mails i retning mod brugeren. Kigger på det senere.

--- code/forgotpwd.php
+++ code/forgotpwd.php
@@ -1,11 +1,25 @@
 <?php
-require_once('forgotpwd.includes.php');
+require_once('includes/forgotpwd.includes.php');
 if(isset($_POST) & !empty($_POST)){
 	$email = mysqli_real_escape_string($conn, $_POST['email']);
 	$sql = "SELECT * FROM `users` WHERE email = '$email'";
 	$res = mysqli_query($conn, $sql);
 	$count = mysqli_num_rows($res);
 	if($count == 1){
+
+	$r = mysqli_fetch_assoc($res);
+	$password = $r['password'];
+	$to = $r['email'];
+	$subject = "Your Recovered Password";
+
+	$message = "Please use this password to login " . $password;
+	$headers = "From : Amigo@AmigoSupport.dk";
+	if(mail($to, $subject, $message, $headers)){
+		echo "Your Password has been sent to your email id";
+	}else{
+		echo "Failed to Recover your password, try again";
+	}
+
 		echo "Send email to user with password";
 	}else{
 		echo "User name does not exist in database";
