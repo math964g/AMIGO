a0136357fb306dcc785f15eba28969380a9d024c
MathiasGN <43538343+math964g@users.noreply.github.com>
2020-04-18 15:57:36 +0200

    Created a spinning block

---
+++ code/js/wofSpin.js
@@ -0,0 +1,26 @@
+console.log('%c wofSpin.js loaded successfully! ', 'color: #32fa3c');
+
+let spinButton = document.getElementById("spinButton");
+let circleRotation = document.getElementsByClassName("wof")[0];
+
+let rotation = 30;
+// Sets a variable to handle our interval
+let spinner = null;
+
+spinButton.addEventListener("click", spinMe);
+
+function spinMe() {
+  console.log("Spin to win bois");
+  // Defining our variable to the interval we want to handle
+  spinner = setInterval(spinDegree, 10);
+}
+
+function spinDegree() {
+  circleRotation.style.transform = "rotate(" + rotation + "deg)";
+  rotation++;
+
+  if (rotation >= 360) {
+    // Using the spinner variable, we handle the setInterval inside the spinMe function
+    clearInterval(spinner);
+  }
+}
