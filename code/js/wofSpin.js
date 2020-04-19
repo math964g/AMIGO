1f03a9d57e9f9c6eee2e1240c845b5ca801f1dab
MathiasGN <43538343+math964g@users.noreply.github.com>
2020-04-19 16:26:38 +0200

    we got a spinning wheel bois

--- code/js/wofSpin.js
+++ code/js/wofSpin.js
@@ -7,15 +7,18 @@ let rotation = 0;
 // Sets a variable to handle our interval
 let spinner = null;
 
+let spinStop = 0;
+
 spinButton.addEventListener("click", spinMe);
 
 // Function which calls for a spin
 function spinMe() {
-  
+  spinStop = Math.floor(Math.random() * 1080) + 360;
+
   // ----------------------------------------------------------
   // Makes it so you can only click the button once
   // IDEA: This should be remove later, when a check is implemented to see if the user is elgible for a spin
-  spinButton.removeEventListener("click", spinMe);
+  // spinButton.removeEventListener("click", spinMe);
   // ----------------------------------------------------------
 
   console.log("Spin to win bois");
@@ -25,12 +28,14 @@ function spinMe() {
 
 // Handles the spinning part
 function spinDegree() {
-  if (rotation >= 360) {
+  console.log(spinStop);
+
+  circleRotation.style.transform = "rotate(" + rotation + "deg)";
+  rotation++;
+
+  if (rotation >= spinStop) {
     // Using the spinner variable, we handle the setInterval inside the spinMe function
     clearInterval(spinner);
     rotation = 0;
   }
-
-  circleRotation.style.transform = "rotate(" + rotation + "deg)";
-  rotation++;
 }
