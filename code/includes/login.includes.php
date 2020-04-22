7be0550ac4150aabce87d3a6f094cbcacb00290c
SurrenderAt20 <lassevfuglsbjerg@gmail.com>
2020-04-22 13:00:22 +0200

    send to feed

---
+++ code/includes/login.includes.php
@@ -0,0 +1,49 @@
+<?php
+
+  session_start();
+
+  $DBserver = "localhost";
+  $DBusername = "root";
+  $DBpassword = "";
+  $DBdatabase = "amigo_db";
+
+  $conn = mysqli_connect($DBserver, $DBusername, $DBpassword, $DBdatabase);
+  if ( mysqli_connect_errno() ) {
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+  if (!isset($_POST['Email'], $_POST['Password'])) {
+    header("Location: ../login.php?error=emptyfields");
+    exit();
+  }
+
+  if ($stmt = $conn->prepare('SELECT Email, Password FROM users WHERE Email = ?')) {
+
+	$stmt->bind_param('s', $_POST['Email']);
+	$stmt->execute();
+
+	$stmt->store_result();
+
+  if ($stmt->num_rows > 0) {
+	$stmt->bind_result($Email, $Password);
+	$stmt->fetch();
+
+
+	  if ($_POST['Password'] === $Password) {
+    header("Location: ../php/eventFeed.php");
+	 }
+
+   else {
+    header("Location: ../login.php?error=WrongPassword");
+    exit();
+	}
+
+
+  } else {
+    header("Location: ../login.php?error=WrongEmail");
+    exit();
+  }
+
+
+	 $stmt->close();
+    }
