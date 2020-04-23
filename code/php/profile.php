817c493c647105b423e2437be655616b9905ee5c
SurrenderAt20 <lassevfuglsbjerg@gmail.com>
2020-04-23 15:19:27 +0200

    Start of profile page

---
+++ code/php/profile.php
@@ -0,0 +1,40 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <link rel="stylesheet" href="../scss/profile.css">
+    <title>Profile Page</title>
+  </head>
+  <body>
+
+    <section class="profileGrid">
+
+      <article class="profileHeadline">
+        <h1>My Profile</h1>
+      </article>
+
+      <article class="profileImg">
+        <img src="../../assets/profile_pic/5.png" alt="profileImgPlaceholder">
+      </article>
+
+      <article class="nameNEducation">
+        <h2>Name</h2>
+        <br>
+        <h3>Education</h3>
+      </article>
+
+      <article class="interest">
+        <select class="selectInterest" name="interest">
+          <option value="placeholder">Chose interest</option>
+          <option value="football">Football</option>
+          <option value="computer">Computer</option>
+          <option value="dancing">Dancing</option>
+          <option value="party">Partying</option>
+        </select>
+      </article>
+
+    </section>
+
+  </body>
+</html>
