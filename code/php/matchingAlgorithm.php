33a71c6062323fd0e4a465eeef936a7e89435ae5
MathiasGN <43538343+math964g@users.noreply.github.com>
2020-04-25 21:34:27 +0200

    fUck this shit - Do it the messy easy wayt instead

--- code/php/matchingAlgorithm.php
+++ code/php/matchingAlgorithm.php
@@ -6,31 +6,62 @@ $ownerEmail = 'john@lennon.com';
 
 require 'dbConnect.php';
 
-$userInformation = [];
-$userInterests = [];
+$loggedInUser = [];
+$allUserInterests = [];
+$tempArray = [];
+$currentArray = "";
 
-$userData = "SELECT * FROM `user_interests` WHERE Email = '$ownerEmail';";
-$result = mysqli_query($conn, $userData);
+
+// Selects the user and their interest from the DB, and insert it into an array
+$userInterestData = "SELECT * FROM `user_interests` WHERE Email = '$ownerEmail';";
+$result = mysqli_query($conn, $userInterestData);
 $resultCheck = mysqli_num_rows($result);
 
 // If we have data in the database, by it being greater than 0, then we run the code
 if ($resultCheck > 0) {
   // We fetch each row of data, and insert it into our $row variable
   while ($row = mysqli_fetch_assoc($result)) {
-    $i = 1;
-    $userInformation[0] = $row['Email'];
-    array_push($userInformation, $row['Interest_ID']);
-    // $userInformation[$i] = $row['Interest_ID'];
-    $i + $i;
-    echo $i;
+    $loggedInUser[0] = $row['Email'];
+    array_push($loggedInUser, $row['Interest_ID']);
   }
 
-  print_r($userInformation);
+  // TODO: Delete this when done with testing
+  // print_r($loggedInUser);
 
 }
 
-else {
-  echo "No purchase history to show";
+$allUsersInterestData = "SELECT * FROM `user_interests`";
+$result = mysqli_query($conn, $allUsersInterestData);
+$resultCheck = mysqli_num_rows($result);
+
+if ($resultCheck > 0) {
+
+  // A good explanation for multidimensional arrays can be found on the next line
+  // https://www.geeksforgeeks.org/multidimensional-arrays-in-php/
+  while ($row = mysqli_fetch_assoc($result)) {
+
+    if ($currentArray != $row['Email']) {
+      $currentArray = $row['Email'];
+
+      $allUserInterests = array(
+
+        $currentArray => array(
+          $row['Interest_ID']
+        ),
+      );
+    }
+
+    else {
+      echo $currentArray . ": Pushing to array under same name - ";
+      // array_push($allUserInterests, $row);
+      // echo "\nPushed value to: $currentArray";
+      // echo $currentArray;
+    }
+    // array_push($allUserInterests, $row);
+  }
+
+  // print_r($allUserInterests);
+
 }
 
 $conn->close();
