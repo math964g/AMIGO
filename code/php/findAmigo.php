c1d3459f84aeb4dd7fb279f20f0a363ab41da77d
MathiasGN <43538343+math964g@users.noreply.github.com>
2020-04-25 16:26:57 +0200

    Made a pulse thing
    It's fucking sick dud

---
+++ code/php/findAmigo.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../scss/findAmigo.css">
+  <title>Find AMIGO</title>
+</head>
+
+<body>
+
+  <div class="topBar">
+
+    <div class="returnButton">
+
+      <div class="returnArrow"></div>
+
+    </div>
+
+    <h1>AMIGO</h1>
+
+  </div>
+
+  <div class="center_container">
+    <div class="pulseBall">
+      <img src="../../assets/ico/user.svg" alt="">
+    </div>
+  </div>
+
+  <?php include 'bottomBar.php'; ?>
+
+</body>
+
+</html>
