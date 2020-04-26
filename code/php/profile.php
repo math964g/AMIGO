6285f4fa6bfaa9b102c37b4d7d23704be8c4534b
SurrenderAt20 <lassevfuglsbjerg@gmail.com>
2020-04-26 12:57:29 +0200

    styling

--- code/php/profile.php
+++ code/php/profile.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
     <link rel="stylesheet" href="../scss/profile.css">
-    <script src="../js/profile.js"></script>
+    <!-- <script src="../js/profile.js"></script> -->
     <title>Profile Page</title>
     <script
 			  src="https://code.jquery.com/jquery-3.5.0.min.js"
@@ -15,35 +15,41 @@
 
     <section class="profileGrid">
 
-      <article class="profileHeadline">
+
+
+      <section class="profileHeadline">
         <h1>My Profile</h1>
-      </article>
+      </section>
 
-      <article class="avatar-upload">
 
+
+      <section class="avatar-upload">
         <div class="avatar-edit">
            <input id="imageUpload" type='file' accept=".png, .jpg, .jpeg" />
            <label for="imageUpload"></label>
         </div>
 
 
+
        <div class="avatar-preview">
            <div id="imagePreview" style="background-image: url(http://i.pravatar.cc/500?img=7);">
            </div>
        </div>
+     </section>
 
-     </article>
 
 
-      <article class="nameNEducation">
+      <section class="nameNEducation">
         <h2>Name</h2>
         <br>
         <h3>Education</h3>
-      </article>
+      </section>
 
-      <article class="interest">
+
+
+      <section class="choseInterest">
         <form id="interestForm" action="profile.includes.php" method="post">
-          <select class="selectInterest" name="interest">
+          <select name="interest" placeholder="lasjdskla">
             <option value="placeholder">Chose interest</option>
             <option value="football">Football</option>
             <option value="computer">Computer</option>
@@ -52,14 +58,32 @@
           </select>
         </form>
 
-        <form action="index.html" method="post">
-          <button form="interestForm" type="submit" name="add-interest">+</button>
-        </form>
-
-
-      </article>
-
+      </section>
     </section>
 
   </body>
 </html>
+
+
+
+
+
+
+
+<!-- <section class="choseInterest">
+  <form id="interestForm" action="profile.includes.php" method="post">
+    <select class="selectInterest" name="interest">
+      <option value="placeholder">Chose interest</option>
+      <option value="football">Football</option>
+      <option value="computer">Computer</option>
+      <option value="dancing">Dancing</option>
+      <option value="party">Partying</option>
+    </select>
+  </form>
+
+  <form action="index.html" method="post">
+    <button form="interestForm" type="submit" name="add-interest">+</button>
+  </form>
+
+
+  </section> -->
