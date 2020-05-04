c3b9015de6ed124eb25dda1941c1fc069db95c48
SurrenderAt20 <lassevfuglsbjerg@gmail.com>
2020-05-04 13:14:08 +0200

    tweaks

--- code/php/loadUserTags.php
+++ code/php/loadUserTags.php
@@ -1,12 +1,14 @@
 <?php
 
-$ownerEmail = 'bingbong@123.dk';
+$ownerEmail = 'awesome@mail.dk';
 
 require 'dbConnect.php';
 
 $userInterest = [];
+$interestDisplay = [];
 
 $userInterestData = "SELECT * FROM `user_interests` WHERE Email = '$ownerEmail';";
+
 $result = mysqli_query($conn, $userInterestData);
 $resultCheck = mysqli_num_rows($result);
 
@@ -19,4 +21,17 @@ if ($resultCheck > 0) {
 }
 print_r($userInterest);
 
+$userInterestName = " SELECT Interest_Name FROM interests ORDER BY Interest_ID";
+$resultInterest = mysqli_query($conn, $userInterestName);
+$resultCheckInterest = mysqli_num_rows($resultInterest);
+
+// If we have data in the database, by it being greater than 0, then we run the code
+if ($resultCheckInterest > 0) {
+  // We fetch each row of data, and insert it into our $row variable
+  while ($row = mysqli_fetch_assoc($resultInterest)) {
+    array_push($interestDisplay, $row['Interest_Name']);
+  }
+}
+// print_r($interestDisplay);
+echo "Interest_ID" . $userInterest[0] . "Interest name" . $interestDisplay[($userInterest[0] - 1)];
  ?>
